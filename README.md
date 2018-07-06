9b89e1c5c91a115563572c86fa1b39d531bc422a
grahamsider <graham.sider@mail.utoronto.ca>
2018-07-06 18:52:35 +0000

    Cleaned up code, updated README.md

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 # JSONPacketParser
-Uses the jsmn parser to parse a json file into a c data structure. This data structure will be used in a system verilog test bench to test the pr region of galapagos. This is a collaboration with @grahamsnider.
+Uses the jsmn parser to parse a json file into a c data structure. This data structure will be used in a system verilog test bench to test the pr region of galapagos. This is a collaboration with @grahamsider.
 
 The jsmn JSON parser can be found here: https://zserge.com/jsmn.html
--- json_to_sv.c
+++ json_to_sv.c
@@ -54,28 +54,12 @@ void readFile(char * path, char * outputstr) {
     return;
 }
 
-
-
-
 // Increment index until token type is 3 (string)
 // or index is > tokSize
 int incUntilString(int index, jsmntok_t * tok, int tokSize) {
-    //printf("INC UNTIL STRING INDEX: %d", index);
-     //printf("token type after flits: %d\n", tok[index].type);
-     //printf("condition 1 b4: %d\n", tok[index].type != 3);
-    //printf("condition 2 b4: %d\n", index < tokSize);
-    while(tok[index].type != STRING_TOK && index < tokSize) {
-        //printf("*index BEFORE = %d\n", index);
-        index++;
-        //printf("condition 1: %d\n", tok[index].type != 3);
-        //printf("condition 2: %d\n", index < tokSize);
-        //printf("*index AFTER = %d\n", index);
-    }
-    //printf("INC UNTIL STRING INDEX: %d", index);
+    while(tok[index].type != STRING_TOK && index < tokSize) index++;
     return(index);
 }
-
-
 
 // Find tokLen for string at given index
 int getTokLen(jsmntok_t * tok, int  index) {
@@ -84,12 +68,7 @@ int getTokLen(jsmntok_t * tok, int  index) {
 
 // Build string from token at given index
 void buildTokStr(jsmntok_t * tok, char * jsonstr, int index, char * str, int tokLen) {
-    //printf("tok[index].start = %d, tok[index].end = %d", tok[index].start, tok[index].end);
-    //printf("string length: %d\n", tokLen);
-    for(int i = 0; i < tokLen; i++) {
-        //printf("jsonstr[tok[index].start + i] = %c\n", jsonstr[tok[index].start + i]);
-        str[i] = jsonstr[tok[index].start + i];
-    }
+    for(int i = 0; i < tokLen; i++) str[i] = jsonstr[tok[index].start + i];
     str[tokLen] = '\0';
     return;
 }
@@ -109,40 +88,32 @@ int getFieldType(char * str) {
 }
 
 
-//checks to see if next string is "flits"
-bool incUntilFlits(int * p_index, jsmntok_t * tok, int tokSize, char * jsonstr){
-    while(*p_index < tokSize){
-        //find next string
+// Check if new packet (ie next string == "flits")
+bool incUntilFlits(int * p_index, jsmntok_t * tok, int tokSize, char * jsonstr) {
+    while(*p_index < tokSize) {
+        // Find next string
         *p_index = incUntilString(*p_index, tok, tokSize);
-
-        //build the string
+        
+        // Build string
         int tokLen = getTokLen(tok, *p_index);
         char str[tokLen];
         buildTokStr(tok, jsonstr, *p_index, str, tokLen);
 
-        printf("incUntilFlits string: %s\n", str);
-
-        //found a new packet, break out of the loop
-        if(strcmp("flits", str) == 0){
-            printf("New Packet!!\n\n");
+        // If new packet, return false
+        if(strcmp("flits", str) == 0) {
             *p_index++;
             return false;
         }
-        //still in the current packet, keep processing flits
-        else{
-            return true;
-        }
-
+        else return true;
     }
 
-    //finished file, stop
+    // Finished file, stop
     return false;
 }
 
 
 // Parse JSON file into its corresponding data structures
-void parseJSON(char * jsonstr){
-
+void parseJSON(char * jsonstr) {
     jsmntok_t tok[MAX_TOKEN_COUNT];
     unsigned int tokSize = sizeof(tok)/sizeof(tok[0]); // = MAX_TOKEN_COUNT ?
 
@@ -152,82 +123,65 @@ void parseJSON(char * jsonstr){
     // Error checking
     jsmnerr_t err = jsmn_parse(&parser, jsonstr,  strlen(jsonstr), tok, tokSize);
     switch(err) {
-        case -1 : printf("ERROR: Not enough tokens were provided\n"); return;
-        case -2 : printf("ERROR: Invalid character inside JSON string\n"); return;
-        case -3 : printf("ERROR: The string is not a full JSON packet, more bytes expected\n"); return;
+        case -1 : fprintf(stderr, "ERROR: Not enough tokens were provided\n"); exit(EXIT_FAILURE);
+        case -2 : fprintf(stderr, "ERROR: Invalid character inside JSON string\n"); exit(EXIT_FAILURE);
+        case -3 : fprintf(stderr, "ERROR: The string is not a full JSON packet, more bytes expected\n"); exit(EXIT_FAILURE);
         default : printf("INFO: Valid JSON packet provided\n");
     }
 
-    //for(int i = 0; i < tokSize; i++) printf("%d: %d\n", i, tok[i].type);
-
-
     int tokLen = 0;
-    struct Packet packetList[NUM_MAX_PACKETS];
     int packetIndex = 0;
+    packet packetList[NUM_MAX_PACKETS];
+
     for(int index = 3;; index++) {
-        struct Packet tempPacket;
         int flitIndex = 0;
+        packet tempPacket;
 
-        while(incUntilFlits(&index, tok, tokSize,jsonstr)){
+        while(incUntilFlits(&index, tok, tokSize,jsonstr)) {
+            flit tempFlit;
 
-            //index++;
-            struct Flit tempFlit;
-
-            //making a packet
-            //while(incUntilFlits(&index,tok,tokSize)){
-
-            //make a single flit
+            // Constructing flits
             for(int i = 0; i < NUMBER_OF_FLIT_FIELDS; i++){
-
                 index = incUntilString(index, tok, tokSize);
-
                 tokLen = getTokLen(tok, index);
-                //printf("tokLen: %d\n", tokLen);
                 char str[tokLen];
-                //printf("index = %d, tok[index].type = %d\n", index, tok[index].type);
                 buildTokStr(tok, jsonstr, index, str, tokLen);
-
+                
                 int dataLen = getTokLen(tok, index+1);
-                char valueStr [dataLen];
-                int value = buildTokInt(tok,jsonstr, index + 1, valueStr, dataLen);
+                char valueStr[dataLen];
+                int value = buildTokInt(tok, jsonstr, index + 1, valueStr, dataLen);
 
-                if(strcmp(str, "data")==0){
+                if(strcmp(str, "data") == 0) {
                     printf("data value: %d\n", value);
                     tempFlit.data = value;
                 }
-                if(strcmp(str, "keep")==0){
+                else if(strcmp(str, "keep") == 0) {
                     printf("keep value: %d\n", value);
                     tempFlit.keep = value;
                 }
-                if(strcmp(str, "last")==0){
+                else if(strcmp(str, "last") == 0) {
                     printf("last value: %d\n", value);
                     tempFlit.last = value;
                 }
                 index++;
-
             }
             tempPacket.flit_list[flitIndex] = tempFlit;
             flitIndex++;
             tempPacket.num_flits = flitIndex;
-
         }
 
         packetList[packetIndex] = tempPacket;
         packetIndex++;
-        if(index >= tokSize){
-             //packetIndex--;
-             break;
-         }
-
-        //index++;
+        if(index >= tokSize) break;
     }
     packetList[packetIndex-1].num_flits--;
-    for(int n = 0; n < packetIndex; n++){
-        for(int f = 0; f < packetList[n].num_flits; f++){
+
+    // Data checking:
+    for(int n = 0; n < packetIndex; n++) {
+        for(int f = 0; f < packetList[n].num_flits; f++) {
             printf("Flit %d of Packet %d data: %d\n", f, n, packetList[n].flit_list[f].data);
         }
     }
-
 }
 
 
@@ -240,7 +194,6 @@ int main(void) {
     char jsonstr[BUFFER_SIZE];
     readFile(JSON_FILE_PATH, jsonstr);
 
-    //printf("%s", jsonstr);
     parseJSON(jsonstr);
 
     return 0;
