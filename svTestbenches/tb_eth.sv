59e0f7c5d2108939cd2f6215a56d3866f0119ac1
Quinn Smith <quinn.smith98@gmail.com>
2018-07-23 20:08:09 +0000

    added dummy_prj to testbench, also added in seperate file for modularity

--- svTestbenches/tb_eth.sv
+++ svTestbenches/tb_eth.sv
@@ -15,6 +15,31 @@ typedef struct {
 
 import "DPI-C" function void parseJSON(input string jsonFilePath, inout packet extPacketList[], inout int ver, inout int logToFile);
 
+
+module dummy_prj( output reg [63:0] stream_out_DATA,
+    output reg [7:0] stream_out_KEEP, 
+    output reg stream_out_LAST,
+    output reg stream_out_VALID,
+    input stream_out_READY,
+    input [63:0] stream_in_DATA,
+    input [7:0] stream_in_KEEP, 
+    input stream_in_LAST,
+    input stream_in_VALID,
+    output reg stream_in_READY
+    ); 
+
+
+always @ (*) begin
+    stream_out_DATA[63:0] <= stream_in_DATA[63:0];
+    stream_out_KEEP[7:0] <= stream_in_KEEP[7:0];
+    stream_out_LAST <= stream_in_LAST;
+    stream_out_READY <= 1'b1;
+end                       
+
+
+endmodule
+
+
 module eth_stimulate
                     (
                      output reg [63:0] stream_out_DATA,
@@ -75,3 +100,9 @@ module eth_stimulate
     `include "ethernet.sv"
     `include "utility.sv"
 endmodule
+
+
+
+    
+
+    
