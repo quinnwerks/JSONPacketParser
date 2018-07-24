3c100146ef5479d2d7cd01f4a17f542c62e150a0
Quinn Smith <quinn.smith98@gmail.com>
2018-07-24 20:26:36 +0000

    added new testbench file, c file. This was done after simulation flow was verified

--- svTestbenches/tb_eth.sv
+++ svTestbenches/tb_eth.sv
@@ -28,6 +28,17 @@ module dummy_prj( output reg [63:0] stream_out_DATA,
     output reg stream_in_READY
     ); 
 
+wire stupid_data[63:0];
+wire stupid_keep[7:0];
+wire stupid_last;
+wire stupid_valid;
+
+assign stupid_data = 64'b69;
+assign stupid_keep = 8'b255;
+assign stupid_last = 1'b1;
+assign stupid_valid = 1'b1;
+assign stupid_ready = 1'b1;
+
 
 always @ (*) begin
     stream_out_DATA[63:0] <= stream_in_DATA[63:0];
@@ -57,14 +68,16 @@ module eth_stimulate
 
     parameter [47:0] MAC_ADDR_FPGA = 48'hfa163e55ca02; 
     parameter [47:0] MAC_ADDR_STIM = 48'h0cc47a88c047; 
+
+
     
     initial begin
 
-        static string jsonFilePath = "./jsonTests/jsonPacketTest_extensive.json";
-        packet extPacketList[0:63];
-        static int ver = 1'd0;
-        static int logToFile = 1'd0;
-        parseJSON(jsonFilePath, extPacketList, ver, logToFile);
+//        static string jsonFilePath = "./jsonTests/jsonPacketTest_extensive.json";
+//        packet extPacketList[0:63];
+//        static int ver = 1'd0;
+//        static int logToFile = 1'd0;
+//        parseJSON(jsonFilePath, extPacketList, ver, logToFile);
     
         stream_in_READY=1;           
         #50
@@ -74,7 +87,9 @@ module eth_stimulate
                                   );
         
         
-        
+       
+
+
         /*
         gen_transaction(.data(64'h0100000100030000), 
                         .keep(8'hff), 
@@ -87,22 +102,34 @@ module eth_stimulate
         */
 
         // Generate a transaction for each flit
-        for(int i = 0; i < 64; ++i) begin
-            for(int j = 0; j < (extPacketList[i].num_flits); ++j) begin
-                gen_transaction(extPacketList[i].flit_list[j].data, extPacketList[i].flit_list[j].keep, extPacketList[i].flit_list[j].last);
-            end
-        end
+//        for(int i = 0; i < 64; ++i) begin
+//            for(int j = 0; j < (extPacketList[i].num_flits); ++j) begin
+//                gen_transaction(extPacketList[i].flit_list[j].data, extPacketList[i].flit_list[j].keep, extPacketList[i].flit_list[j].last);
+//            end
+//        end
+
+
 
 
          
     end
+
+    dummy_prj verilog_for_dummies(
+        .stream_out_DATA(stream_out_DATA), 
+        .stream_out_KEEP(stream_out_KEEP), 
+        .stream_out_LAST(stream_out_LAST),
+        .stream_out_VALID(stream_in_VALID), 
+//        .stream_in_DATA(stream_in_DATA),
+//        .stream_in_KEEP(stream_in_KEEP),
+//        .stream_in_LAST(stream_in_LAST),
+//        .stream_in_READY(stream_out_READY)
+        .stream_in_DATA(stupid_data),
+        .stream_in_KEEP(stupid_keep),
+        .stream_in_LAST(stupid_last),
+        .stream_in_READY(stupid_ready)
+    );
     
     `include "ethernet.sv"
     `include "utility.sv"
+
 endmodule
-
-
-
-    
-
-    
