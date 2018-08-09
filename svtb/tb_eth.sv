1541faab0536f8a47eaf523f8fcc6cc4fb29d0dd
grahamsider <graham.sider@mail.utoronto.ca>
2018-08-09 19:11:45 +0000

    Added new testbenches from galapagos for easier use (credit to @tarafdar), depricated old tb files

--- svtb/tb_eth.sv
+++ svtb/tb_eth.sv
@@ -2,59 +2,44 @@
 
 //import ethernet::*;
 
+
 module eth_stimulate
-    (
-    output reg [63:0] stream_out_DATA,
-    output reg [7:0] stream_out_KEEP, 
-    output reg stream_out_LAST,
-    output reg stream_out_VALID,
-    input stream_out_READY,
-    input [63:0] stream_in_DATA,
-    input [7:0] stream_in_KEEP, 
-    input stream_in_LAST,
-    input stream_in_VALID,
-    output reg stream_in_READY
-    );
+                    (
+                     output reg [63:0] stream_out_DATA,
+                     output reg [7:0] stream_out_KEEP, 
+                     output reg stream_out_LAST,
+                     output reg stream_out_VALID,
+                     input stream_out_READY,
+                     input [63:0] stream_in_DATA,
+                     input [7:0] stream_in_KEEP, 
+                     input stream_in_LAST,
+                     input stream_in_VALID,
+                     output reg stream_in_READY
+                    );
 
 
     parameter [47:0] MAC_ADDR_FPGA = 48'hfa163e55ca02; 
-    parameter [47:0] MAC_ADDR_STIM = 48'h0cc47a88c047;
-
-    reg [63:0] out_data;
-    reg [7:0] out_keep;
-    reg out_last;
+    parameter [47:0] MAC_ADDR_STIM = 48'h0cc47a88c047; 
+    
 
     initial begin
-
-        stream_in_READY = 1'b1;           
+        stream_in_READY=1;           
         #50
-        ethernet_header
-            (
-            .mac_addr_dst(MAC_ADDR_FPGA),
-            .mac_addr_src(MAC_ADDR_STIM), 
-            .dst(8'h00)
-            );
- 
+        ethernet_header(.mac_addr_dst(MAC_ADDR_FPGA),
+                                  .mac_addr_src(MAC_ADDR_STIM), 
+                                  .dst(8'h00)
+                                  );
+        gen_transaction(.data(64'h0100000100030000), 
+                        .keep(8'hff), 
+                        .last(1'b0)
+                    );
+        gen_transaction(.data(64'h5073930200000000), 
+                        .keep(8'h0f), 
+                        .last(1'b1)
+                    );
+         
     end
-
-    fileIO conv_json
-        (
-        .out_data(out_data),
-        .out_keep(out_keep),
-        .out_last(out_last)
-        );
-
-    always@(*) begin
-        gen_transaction
-            (
-                .data(out_data),
-                .keep(out_keep),
-                .last(out_last)
-            );
-    end
-
-    `include "file_io.sv"
-    `include "utility.sv"
+    
     `include "ethernet.sv"
-
+    `include "utility.sv"
 endmodule
